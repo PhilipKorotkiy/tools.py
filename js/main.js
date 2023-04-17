8df39997f51dde873101bebe54be1ca2a06f3f00
BluCat <blucatnight@gmail.com>
2023-04-17 09:14:34 -0700

    Major overhual

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ function buttonanim(id,op) {
         opacity: op
     },200)
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+function lod() {
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
+}}
